6580a75b703f12eb421eab2431d797d09f3f09c4
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2021-03-05 11:02:03 +0100

    funkar inte

--- src/komplex.java
+++ src/komplex.java
@@ -2,6 +2,24 @@ public class komplex {
     public static void main(String[] args){
         komplex a = new komplex (3.0,2.0);
         komplex b = new komplex (4.0,1.0);
+
+        komplex c = a.add(b);
+        komplex d = (7,3);
+        System.out.println(c);
+        if (c.equals(d)) {
+            System.out.println("add funkar");
+        }
+        c = a.multiply(b);
+        d = new Komplex(10,11);
+
+        System.out.println(c);
+        if (c.equals(d)) {
+            System.out.println("multiply funkar!!");
+        }
+
+        System.out.println("Argumentet till 3+2i är " + a.arg());
+        if (a.arg() == Math.atan(2.0/3)) {
+            System.out.println("arg() funkar!!");
+        }
     }
 }
-    
